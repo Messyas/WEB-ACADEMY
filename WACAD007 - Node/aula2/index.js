6dab0a97047668e3bb6ebfd99dfe0f59f61f4dc1
Messyas <messyasgoisfranca@gmail.com>
2025-05-10 08:57:47 -0400

    aula2

---
+++ WACAD007 - Node/aula2/index.js
@@ -0,0 +1,21 @@
+const http = require('http')
+const fs = require('fs')
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+
+const PORT = process.env.PORT ?? 9999
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html;charset=utf-8'})
+    fs.readFile("./index.html", (err, content) => {
+        if(err) res.write(err);
+        else fs.write(content);
+        res.end()
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`O servidor esta rodando na porta ${PORT}`)
+});
