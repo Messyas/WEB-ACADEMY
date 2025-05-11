3eae644c54fae0e33c1fe89903a23301232c5e41
Messyas <messyasgoisfranca@gmail.com>
2025-05-11 17:23:21 -0400

    ex 1

--- WACAD007 - Node/aula2/index.js
+++ WACAD007 - Node/aula2/index.js
@@ -23,3 +23,30 @@ server.listen(PORT, () => {
 //instalar package json e package lock
 
 //
+const http = require("http");
+const dotenv = require("dotenv");
+const fs = require("fs")
+
+dotenv.config({ path: `.env.${process.env.PORTNODE_ENV}`});
+
+const PORT = process.env.PORT ?? 8888
+
+console.log(process.env)
+
+fs.readdir(nomeDir, (err, arquivos) => {
+    if (err) console.log(err);
+    console.log("\n Nome do diretorio:");
+    arquivos.forEach(arquivo => {
+        console.log(arquivo);
+    });
+});
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"content-type":"text/html;charset=utf8"});
+    res.write("Web academy");
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`ervidor rodando na porta ${PORT}`);
+});
--- WACAD007 - Node/node1/index.js
+++ WACAD007 - Node/node1/index.js
@@ -1,27 +1,20 @@
-const http = require("http");
-const dotenv = require("dotenv");
-const fs = require("fs")
+const http = require('http');
+const fs = require('fs');
 
-dotenv.config({ path: `.env.${process.env.PORTNODE_ENV}`});
+const pathdir = process.argv[2];
 
-const PORT = process.env.PORT ?? 8888
-
-console.log(process.env)
-
-fs.readdir(nomeDir, (err, arquivos) => {
-    if (err) console.log(err);
-    console.log("\n Nome do diretorio:");
-    arquivos.forEach(arquivo => {
-        console.log(arquivo);
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type':'text/html;charset=utf-8'});
+    fs.readdir(pathdir, (err, files) => {
+        if (err) console.log(err);
+        else {
+            console.log("\n Arquivos contidos no diretorio");
+            files.forEach(file => {
+                console.log(file);
+            });
+        }
+        res.end();
     });
 });
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-type":"text/html;charset=utf8"});
-    res.write("Web academy");
-    res.end();
-});
-
-server.listen(PORT, () => {
-    console.log(`ervidor rodando na porta ${PORT}`);
-});
+server.listen(2122);
