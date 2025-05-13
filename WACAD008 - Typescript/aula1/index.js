29209278b697e5ff5dfed15b1ab0db2451f16f29
Messyas <messyasgoisfranca@gmail.com>
2025-05-13 19:55:34 -0400

    notes

--- WACAD008 - Typescript/aula1/index.js
+++ WACAD008 - Typescript/aula1/index.js
@@ -1,5 +1,6 @@
+"use strict";
 //interface no typescript
 //Similarmente ao java, serve para definir os comportamentos esperados que um objeto deve ter, e alem disso,
 //ele tambem permite a definicao de atributos.
-let nomes: string[] = ['hohoho', 'guts', 'saitama'];
+let nomes = ['hohoho', 'guts', 'saitama'];
 console.log(nomes);
