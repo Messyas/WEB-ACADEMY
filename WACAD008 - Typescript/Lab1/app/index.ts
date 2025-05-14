1f47e23d379232480bac4a3f9648f904d3b12fed
Messyas <messyasgoisfranca@gmail.com>
2025-05-14 10:06:17 -0400

    logica de add tasks

--- WACAD008 - Typescript/Lab1/app/index.ts
+++ WACAD008 - Typescript/Lab1/app/index.ts
@@ -1,14 +1,29 @@
 //tupla para representar os registros
-type Task = [number, string, Date, Date, string];
+type Task = [string, string, Date, Date, string];
 
 let taskList: Task[] = [];
 
 function addTask(): void {
-    const title = (document.getElementById('title') as HTMLInputElement).value;
-    const iniDate = new Date((document.getElementById('init-date') as HTMLInputElement).value);
-    const finalDate = new Date((document.getElementById('final-date') as HTMLInputElement).value);
-    const description = (document.getElementById('title') as HTMLInputElement).value;
+  const title = (document.getElementById("title") as HTMLInputElement).value;
+  const iniDate = new Date(
+    (document.getElementById("init-date") as HTMLInputElement).value
+  );
+  const finalDate = new Date(
+    (document.getElementById("final-date") as HTMLInputElement).value
+  );
+  const description = (
+    document.getElementById("description") as HTMLInputElement
+  ).value;
 
-    const id = 
+  const id = crypto.randomUUID();
 
+  const newTask: Task = [id, title, iniDate, finalDate, description];
+  taskList.push(newTask);
+
+  console.log("Tarefa adiocionada:", newTask);
 }
+
+(document.getElementById("button-add") as HTMLButtonElement).addEventListener(
+  "click",
+  addTask
+);
