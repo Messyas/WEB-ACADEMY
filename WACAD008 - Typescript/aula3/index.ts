7788a61e1f0db70dc3d860cf2024372c3e38f331
Messyas <messyasgoisfranca@gmail.com>
2025-05-17 10:05:22 -0400

    notes aula 3

---
+++ WACAD008 - Typescript/aula3/index.ts
@@ -0,0 +1,38 @@
+// programar para uma interface ao inves de uma classe
+// por exemplo, um array de musicos, ao inves de criar a partir de varias classes diferentes, use uma
+// interface musico e armazene no array musicos com especilizacoes diferentes.
+
+//olhar links dos slides 3
+
+//class peeps
+//demostracao de atributos estaticos
+//static pertence a classe, e nao ao objetos delas
+//util pra contar a quantidade de objetos criados por exemplo
+
+//templates
+//fazer git com funcoes de guarda prontas com templates pra facilitar desenvolvimento
+//template de interfaces
+
+//leasson 09
+// aula 3
+// tem aula gravada
+//exemplo de tipos utilitarios para funcoes etc.
+
+//parameters
+//promise
+//tipo awaited
+
+//da pra dizer que um usuario vai pertencer a um tipo utilitario promises
+
+
+// melhores padroes de projeto 
+// padroes estruturais e comportamente
+
+//padrao Adapter -> tranforma uma interface em outra -> ele extende uma interfece usando codigo de outra
+//singleton -> conexao com apits - como a de mercado financeiro -> ele garante que somente uma intancia existe 
+//Observer -> backend para coisas internas em servicos
+//strategy -> tarefa que muda de acordo com a entrada -> 
+//facade -> 
+//chain of responsibility -> bom para front-end
+
+//lab3 - get set, T <--
