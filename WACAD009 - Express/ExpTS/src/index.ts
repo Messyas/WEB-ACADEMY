2c0677e3568d4256dfe528bdbabc5c8ae7aab32b
Messyas <messyasgoisfranca@gmail.com>
2025-05-20 20:10:04 -0400

    outros metodos http

--- WACAD009 - Express/ExpTS/src/index.ts
+++ WACAD009 - Express/ExpTS/src/index.ts
@@ -1,21 +1,31 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
-import validateEnv from "./utils/validateEnv";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import validateEnv from './utils/validateEnv';
 
-dotenv.config()
+dotenv.config();
 validateEnv(); //faz a validacao das variaveis de ambiente
 
 const PORT = process.env.PORT ?? 6969;
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world!');
 });
 
-app.get("/about", (req: Request, res: Response) => {
-    res.send("about page");
+app.get('/about', (req: Request, res: Response) => {
+  res.send('about page');
+});
+
+app.post('/', (req: Request, res: Response) => {
+    res.write("sei la")
+  console.log('Requisição POST no /');
+});
+
+app.use((req: Request, res: Response) => {
+    res.write("hohoo")
+  console.log('Executado por toda requisição');
 });
 
 app.listen(PORT, () => {
-    console.log(`Port running on port: ${PORT}`);
+  console.log(`Port running on port: ${PORT}`);
 });
