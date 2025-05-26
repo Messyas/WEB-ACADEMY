f9d3fc9a9d2e4709ed6f7534cd9890e533ce98a1
Messyas <messyasgoisfranca@gmail.com>
2025-05-26 00:08:57 -0400

    new add method

--- WACAD008 - Typescript/Lab3/src/models/CarrinhoDeCompras.ts
+++ WACAD008 - Typescript/Lab3/src/models/CarrinhoDeCompras.ts
@@ -1,14 +1,23 @@
 import { CarrinhoDeComprasInterface } from "./interfaces/CarrinhoDeComprasInterface";
+import { ItemCarrinho } from "./interfaces/ItemCarrinho";
 import { Produto } from "./interfaces/ProdutoInterface";
 
 //classe generica
 export class CarrinhoDeCompras<T extends Produto>
   implements CarrinhoDeComprasInterface<T>
 {
-  public produtos: T[] = [];
+  public itens: Map<string, ItemCarrinho<T>> = new Map();
 
-  adicionar(item: T): void {
-    this.produtos.push(item);
+  adicionar(produto: T, quantidade: number = 1): void {
+    if (quantidade <= 0) return;
+
+    if (this.itens.has(produto.id)) {
+      const itemExistente = this.itens.get(produto.id)!;
+      itemExistente.quantidade += quantidade;
+    } else {
+      this.itens.set(produto.id, { produto, quantidade });
+    }
+    console.log('produto adicionado');
   }
 
   removerPorIndice(indice: number): boolean {
