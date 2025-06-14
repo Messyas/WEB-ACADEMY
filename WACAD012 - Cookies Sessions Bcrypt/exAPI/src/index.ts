9970c3f466c224d91a1546dd85da58102c31a93a
Messyas <messyasgoisfranca@gmail.com>
2025-06-14 10:23:35 -0400

    Modulo bicript etc

---
+++ WACAD012 - Cookies Sessions Bcrypt/exAPI/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './router/router';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT ?? 3366;
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
