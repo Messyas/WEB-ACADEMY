d5642f9941033f876b730b0e96004b8e039c8d26
Messyas <messyasgoisfranca@gmail.com>
2025-06-17 15:12:32 -0400

    fix

---
+++ WACAD011 - Banco de dados/lab1/loja.sql
@@ -0,0 +1,74 @@
+CREATE TABLE cliente (
+  CPF VARCHAR(11) NOT NULL,
+  primeiroNome VARCHAR(100) NOT NULL,
+  meioNome VARCHAR(100) NOT NULL,
+  ultimoNome VARCHAR(100) NOT NULL,
+  nascimento DATE NOT NULL,
+  telefone VARCHAR(255) NOT NULL,
+  PRIMARY KEY (CPF)
+);
+
+CREATE TABLE endereco (
+  id INT NOT NULL AUTO_INCREMENT,
+  numero INT NOT NULL,
+  rua VARCHAR(100) NOT NULL,
+  bairro VARCHAR(100) NOT NULL,
+  cidade VARCHAR(100) NOT NULL,
+  estado VARCHAR(100) NOT NULL,
+  cliente_cpf VARCHAR(11) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (cliente_cpf) REFERENCES cliente (CPF)
+);
+
+CREATE TABLE categoria (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE subcategoria (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  categoria_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (categoria_id) REFERENCES categoria (id)
+);
+
+CREATE TABLE produto (
+  id INT NOT NULL AUTO_INCREMENT,
+  modelo VARCHAR(100) NOT NULL,
+  fabricante VARCHAR(100) NOT NULL,
+  preco DECIMAL(10,2) NOT NULL,
+  quantidade INT NOT NULL,
+  subcategoria_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (subcategoria_id) REFERENCES subcategoria (id)
+);
+
+CREATE TABLE compra (
+  id INT NOT NULL AUTO_INCREMENT,
+  totalCompra DECIMAL(10,2) NOT NULL,
+  hora TIME NOT NULL,
+  dia DATE NOT NULL,
+  desconto DECIMAL(10,2) NOT NULL,
+  cliente_cpf VARCHAR(11) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (cliente_cpf) REFERENCES cliente (cpf)
+);
+
+CREATE TABLE lista (
+  id INT NOT NULL AUTO_INCREMENT,
+  produto_id INT NOT NULL,
+  compra_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (produto_id) REFERENCES produto (id),
+  FOREIGN KEY (compra_id) REFERENCES compra (id)
+);
+
+CREATE TABLE serie (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  produto_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (produto_id) REFERENCES produto (id)
+);
