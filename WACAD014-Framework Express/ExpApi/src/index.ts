0ce61c97688421bb8d291cf31eaeff4d2b06231a
Messyas <messyasgoisfranca@gmail.com>
2025-06-28 10:59:26 -0400

    cookies config

--- WACAD014-Framework Express/ExpApi/src/index.ts
+++ WACAD014-Framework Express/ExpApi/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import router from "./router/index";
 
@@ -8,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT ?? 3366;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(router);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
