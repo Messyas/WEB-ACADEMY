2ba1924babecd7e6fbbc84fae091f2cb8fb16667
Messyas <messyasgoisfranca@gmail.com>
2025-07-20 23:52:19 -0400

    Ex gen1 - final

--- WACAD015-GerenciamentoDeEstado/wa-loja-context-api-base-main/src/app/components/ListagemProdutos/ListagemProdutos.tsx
+++ WACAD015-GerenciamentoDeEstado/wa-loja-context-api-base-main/src/app/components/ListagemProdutos/ListagemProdutos.tsx
@@ -1,11 +1,6 @@
-// app/components/ListagemProdutos/ListagemProdutos.tsx
-
 import CardProduto from "../CardProduto/CardProduto";
-// Assumindo que o nome do componente de resumo seja este
 import ListagemFavoritos from "../ListagemFavoritos/ListagemFavoritos";
 
-// 1. Remova 'favoritos' e 'setFavoritos' da interface.
-// O componente só precisa saber da lista de produtos a ser exibida.
 interface IListagemProdutosProps {
   produtos: Produto[];
 }
@@ -15,12 +10,7 @@ export default function ListagemProdutos({ produtos }: IListagemProdutosProps) {
     <div className="row row-cols-1 row-cols-lg-2">
       <div className="col-lg-9">
         <h5 className="mb-3">Produtos disponíveis:</h5>
-
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
-          {/*
-            2. Chame o CardProduto sem passar as props de favoritos.
-               Ele agora pega tudo o que precisa do contexto.
-          */}
           {produtos.map((produto) => (
             <CardProduto key={produto.id} produto={produto} />
           ))}
@@ -28,12 +18,6 @@ export default function ListagemProdutos({ produtos }: IListagemProdutosProps) {
       </div>
 
       <div className="col-lg-3">
-        {/*
-          3. Renderize o componente de favoritos sem passar nenhuma prop.
-             Ele também é autossuficiente agora.
-             Estou usando o nome 'ListagemFavoritos' que corrigimos no primeiro passo.
-             Se o nome for 'ResumoFavoritos', apenas substitua.
-        */}
         <ListagemFavoritos />
       </div>
     </div>
